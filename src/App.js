c79da48c5f5971614d6be51ef4bb209813451c96
Joy Cruz <jocruz4966@gmail.com>
2023-02-20 17:06:01 +0000

    Reorganize the folders project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Navbar from './component/Navbar';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from './component/Home';
@@ -28,19 +26,6 @@ function App() {
          <Route path='/signup' component ={SignUp} exact><SignUp/></Route>
        </Switch>
       </Router>
-      
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Edit <code>src/App.js</code> and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer" >Learn React
-        </a>
-      </header>
-
     </div>
   );
 }
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,21 +4,24 @@ import "./navbar.css"
 
 const Navbar = () => {
     return(
-        <section className="nav-bar">
+        <section className="navbar">
            <h1>Welcome to Navbar Page</h1>
 
            <nav className="nav-bar">
-            <h3 className="logo"> Logo</h3>
-            <ul className="nav-links">
-              <Link to="/"><li>Home</li></Link>
-              <Link to="/services"><li>Services</li></Link>
-              <Link to="/products"><li>Products</li></Link>
-              <Link to="/cart"><li>Cart</li></Link>
-              <Link to="/delivery"><li>Delivery</li></Link>
-              <Link to="/contact"><li>Contact</li></Link>
-              <Link to="/signup"><li>Sign UP</li></Link>
-            </ul>
+            <div className="container">
+             <h3 className="logo"> Logo</h3>
+              <ul className="nav-links">
+               <Link to="/"><li>Home</li></Link>
+               <Link to="/services"><li>Services</li></Link>
+               <Link to="/products"><li>Products</li></Link>
+               <Link to="/cart"><li>Cart</li></Link>
+               <Link to="/delivery"><li>Delivery</li></Link>
+               <Link to="/contact"><li>Contact</li></Link>
+               <Link to="/signup"><li>Sign UP</li></Link>
+             </ul>
+            </div>
            </nav>
+           
         </section>
     );
 }
