8d63a92521d7bd0413c8564ee5e8f5d9aad220fd
Joy Cruz <jocruz4966@gmail.com>
2023-02-22 00:34:49 +0000

    Rearrange HTML structure for JSX

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,42 +1,34 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "./navbar.css"
-import { FaBars } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import "../App.css"
+import { FaBars, FaSearch } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 
 function Navbar() {
   const [mobile, setMobile] = useState(false);
   
   return (
-    <nav className="nav-bar">
-     
-        <h3 className="logo">Logo</h3>
 
+    <div className="header">
+
+      <form  action="submit" >
+       <input  type="text" placeholder="Givaway..." />
+       <button ><FaSearch /></button>
+      </form>
+      
+     <nav className="nav-bar">
+       <h3 className="logo">Logo</h3>
         <ul 
         className={ mobile ? "nav-links-mobile" : "nav-links"}
         onClick= {() => {setMobile(false); }}
         >
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-          <Link to="/services">
-            <li>Services</li>
-          </Link>
-          <Link to="/products">
-            <li>Products</li>
-          </Link>
-          <Link to="/cart">
-            <li>Cart</li>
-          </Link>
-          <Link to="/delivery">
-            <li>Delivery</li>
-          </Link>
-          <Link to="/contact">
-            <li>Contact</li>
-          </Link>
-          <Link to="/signup">
-            <li>Sign UP</li>
-          </Link>
+          <NavLink to="/"><li>Home</li></NavLink>
+          <NavLink to="/services"><li>Services</li></NavLink>
+          <NavLink to="/products"><li>Products</li></NavLink>
+          <NavLink to="/cart"><li>Cart</li></NavLink>
+          <NavLink to="/delivery"><li>Delivery</li></NavLink>
+          <NavLink to="/contact"><li>Contact</li></NavLink>
+          <NavLink to="/signup"><li>Sign UP</li></NavLink>
         </ul>
     <button 
        className="mobile-menu-icon"
@@ -44,8 +36,9 @@ function Navbar() {
     >
       {mobile ? <ImCross /> :  <FaBars />}
     </button>
-      
     </nav>
+
+  </div>
   );
 }
 
