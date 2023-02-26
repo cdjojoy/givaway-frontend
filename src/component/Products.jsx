f11f6f2fa63bbda8bbd04e1007a64a20282bc76a
Joy Cruz <jocruz4966@gmail.com>
2023-02-26 15:19:56 +0000

    Create a Fake JSON Data file in Public folder

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import PhotoList from "./PhotoList";
+import Products from './data/products.json';
+
 
 
  const Products = () => {
@@ -9,9 +11,10 @@ import PhotoList from "./PhotoList";
     
 
     useEffect( () => {
-        fetch("https://dummyjson.com/products")
-          .then(response => response.json())
-          .then(products => setProducts(products.products));
+       // fetch("https://dummyjson.com/products")
+        //  .then(response => response.json())
+        //  .then(products => setProducts(products.products));
+        setProducts(products.products.json)
     }, []);
 
     //const productsComp =
@@ -23,6 +26,10 @@ return (
           <PhotoList
             key={product.id}
             product={product}
+            images={product.images}
+            title={product.title}
+            description={product.description}
+            price={product.price}
           />
         );
     })}
